fa45c1fcce305937ad127ccb1fc5a5a552ed93a3
Manulakshan <manulakshan.y@gmail.com>
2024-06-04 00:28:31 +0530

    username on navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React , {useEffect } from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home'; 
 import  Checkout from './Checkout'; 
 import  Login from './Login';
-
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 import {BrowserRouter as Router, Routes ,Route } from "react-router-dom";
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() =>{
+      // only runs once app component loads
+
+      auth.onAuthStateChanged(authUser =>{
+        console.log('The User is >>>',authUser);
+
+        if(authUser){
+          //if user is logged /was logged in
+          dispatch({
+            type:'SET_USER',
+            user: authUser
+          })
+        }else{
+          //the user is logged out
+          dispatch({
+            type:'SET_USER',
+            user: null
+          })
+
+        }
+
+      })
+  },[])
+
   return (
     //BEM
     <Router>
